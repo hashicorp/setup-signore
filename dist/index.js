afdfc8c7357de714e289f83434ca8137ec05349a
Caleb Albers <calbers@hashicorp.com>
2022-12-01 13:15:14 -0800

    Exempt dist/** via .copywrite.hcl config

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
