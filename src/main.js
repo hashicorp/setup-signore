a1a1dcc8cc5b8b8404f1b000fb1fb0d0b96f43f6
Antony Saba (work) <111310710+asaba-hashi@users.noreply.github.com>
2025-03-25 16:00:29 -0600

    Updating to match current github actions template (#98)

--- src/main.js
+++ src/main.js
@@ -5,15 +5,15 @@
 
 'use strict'
 
-const fs = require('fs').promises
-const os = require('os')
-const path = require('path')
-const crypto = require('crypto')
+import fs from 'fs/promises'
+import os from 'os'
+import path from 'path'
+import crypto from 'crypto'
 
-const core = require('@actions/core')
-const tc = require('@actions/tool-cache')
+import * as core from '@actions/core'
+import tc from '@actions/tool-cache'
 
-const octokit = require('./octokit')
+import octokit from './octokit.js'
 
 const owner = 'hashicorp'
 const repo = 'signore'
@@ -21,7 +21,7 @@ const repo = 'signore'
 // adapted from setup-terraform
 // arch in [arm, x32, x64...] (https://nodejs.org/api/os.html#os_os_arch)
 // return value in [x86_64, 386, arm]
-function mapArch (arch) {
+function mapArch(arch) {
   const mappings = {
     x32: '386',
     x64: 'x86_64'
@@ -32,14 +32,14 @@ function mapArch (arch) {
 // adapted from setup-terraform
 // os in [darwin, linux, win32...] (https://nodejs.org/api/os.html#os_os_platform)
 // return value in [darwin, linux, windows]
-function mapOS (os) {
+function mapOS(os) {
   const mappings = {
     win32: 'windows'
   }
   return mappings[os] || os
 }
 
-async function run () {
+export async function run() {
   try {
     const expectedArchiveChecksum = core.getInput('archive-checksum')
     const githubToken = core.getInput('github-token')
@@ -57,30 +57,48 @@ async function run () {
     // if we don't have specific version, get latest release
     let releaseToDownload
     if (version === 'latest' || !version) {
-      releaseToDownload = (await client.rest.repos.getLatestRelease({ owner, repo })).data
+      releaseToDownload = (
+        await client.rest.repos.getLatestRelease({ owner, repo })
+      ).data
     } else {
-      releaseToDownload = (await client.rest.repos.getReleaseByTag({ owner, repo, tag: version })).data
+      releaseToDownload = (
+        await client.rest.repos.getReleaseByTag({ owner, repo, tag: version })
+      ).data
     }
     const tag = releaseToDownload.tag_name
     core.setOutput('version', tag)
 
     // i.e. signore_0.1.2_darwin_x86_64.tar.gz
     const expectedAssetName = `${repo}_${tag.replace('v', '')}_${platform}_${arch}${archiveSuffix}`
-    const assetToDownload = releaseToDownload.assets.find(asset => asset.name === expectedAssetName)
+    const assetToDownload = releaseToDownload.assets.find(
+      (asset) => asset.name === expectedAssetName
+    )
     if (assetToDownload === undefined) {
-      throw new Error(`Unable to find asset matching ${expectedAssetName} in the ${tag} release`)
+      throw new Error(
+        `Unable to find asset matching ${expectedAssetName} in the ${tag} release`
+      )
     }
 
     const url = assetToDownload.url
     const auth = 'token ' + (githubToken || process.env.GITHUB_TOKEN)
 
     core.debug(`Downloading ${repo} release from ${url}`)
-    const downloadedArchive = await tc.downloadTool(url, undefined, auth, { accept: 'application/octet-stream' })
+    const downloadedArchive = await tc.downloadTool(url, undefined, auth, {
+      accept: 'application/octet-stream'
+    })
 
     if (expectedArchiveChecksum !== '') {
-      const downloadedArchiveChecksum = crypto.createHash('sha256').update(await fs.readFile(downloadedArchive)).digest('hex')
+      const downloadedArchiveChecksum = crypto
+        .createHash('sha256')
+        .update(await fs.readFile(downloadedArchive))
+        .digest('hex')
       if (expectedArchiveChecksum !== downloadedArchiveChecksum) {
-        throw new Error(`Checksum mismatch: ${downloadedArchiveChecksum} does not match expected checksum ${expectedArchiveChecksum}`)
+        core.error(
+          `Checksum mismatch: ${downloadedArchiveChecksum} does not match expected checksum ${expectedArchiveChecksum}`
+        )
+        throw new Error(
+          `Checksum mismatch: ${downloadedArchiveChecksum} does not match expected checksum ${expectedArchiveChecksum}`
+        )
       }
     }
 
@@ -104,7 +122,10 @@ async function run () {
       let configContent = ''
       configContent += signer ? `signer: ${signer}\n` : ''
       await fs.mkdir(path.join(os.homedir(), '.signore'))
-      await fs.writeFile(path.join(os.homedir(), '.signore', 'config.yaml'), configContent)
+      await fs.writeFile(
+        path.join(os.homedir(), '.signore', 'config.yaml'),
+        configContent
+      )
     }
 
     core.debug('success: signore has been set up!')
@@ -112,5 +133,3 @@ async function run () {
     core.setFailed(error.message)
   }
 }
-
-module.exports = run
